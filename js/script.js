7accf81d009a7fa5860ea3300f423cfadbde9fc5
Łukasz Żurawiecki <VonKranich46@gmail.com>
2020-07-02 14:36:20 +0200

    Added random generation of answer

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ let playerMover = "papier";
 printMessage(
   "Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!"
 );
+
+let randomFraction = Math.random();
+
+printMessage("Wylosowany ułamek to: " + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage("Ułamek pomnożony przez 3 i powiększony o 1: " + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage("Liczba po zaokrągleniu w dół to: " + roundNumber);
